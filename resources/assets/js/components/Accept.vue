1df3ffa892d6f7a85eab0cdffd689922f944d0c2
Stefan Gouldson <sgouldson@CASPIAN71.caspian.local>
2021-01-19 17:51:01 +0000

    global event bus

--- resources/assets/js/components/Accept.vue
+++ resources/assets/js/components/Accept.vue
@@ -12,6 +12,7 @@
 
 
 <script>
+import EventBus from '../event-bus'
 export default {
     props: ['answer'],
 
@@ -20,6 +21,12 @@ export default {
             isBest: this.answer.is_best,
             id: this.answer.id
         }
+    },
+
+    created(){
+        EventBus.$on('accepted', id=>{
+            this.isBest = (id === this.id);
+        })
     },
 
     methods: {
@@ -32,6 +39,7 @@ export default {
                 });
 
                 this.isBest = true;
+                EventBus.$emit('accepted', this.id);
             })
         }
     },
